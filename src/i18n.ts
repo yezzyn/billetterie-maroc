6cbed6e7e0454debe5bca02d464ca92579796603
yezzyn <yezzyn@gmail.com>
2026-08-23 17:40:13 +0100

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,8 @@ import { getRequestConfig } from 'next-intl/server';
 import { locales, defaultLocale } from './i18n/config';
 
 export default getRequestConfig(async ({ locale }) => {
-  const validLocale = locales.includes(locale as any) ? locale : defaultLocale;
+  // On force TypeScript à comprendre que validLocale est TOUJOURS une string
+  const validLocale: string = (locale && locales.includes(locale as any)) ? locale : defaultLocale;
   
   return {
     locale: validLocale,
