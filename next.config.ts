8f426a7773677feaddc64645fb72210a1897ebc4
yezzyn <yezzyn@gmail.com>
2026-08-23 17:29:30 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 import type { NextConfig } from 'next';
 
-// 1. On indique à next-intl où se trouve la configuration
+// Pointe vers le fichier unifié que nous venons de créer
 const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 
-// 2. On définit une configuration Next.js vide et propre pour éviter les erreurs TypeScript
-const nextConfig: NextConfig = {
-  // Nous retirons temporairement la configuration 'images' 
-  // qui bloque le build à cause de la strictesse de TypeScript.
-  // L'application fonctionnera parfaitement sans.
-};
+const nextConfig: NextConfig = {};
 
-// 3. On exporte le tout
 export default withNextIntl(nextConfig);
