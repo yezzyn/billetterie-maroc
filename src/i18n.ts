fefc6ef6a1b33032277b3d751fd18389fcb7aa05
yezzyn <yezzyn@gmail.com>
2026-08-23 16:45:38 +0100

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,4 @@
 import { getRequestConfig } from 'next-intl/server';
-import { locales } from './i18n/config';
 
 export default getRequestConfig(async ({ locale }) => ({
   messages: (await import(`../messages/${locale}.json`)).default
