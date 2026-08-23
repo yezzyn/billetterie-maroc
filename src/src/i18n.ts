e73124d3f3de8e1928768bbd45e28c43ce905e68
yezzyn <yezzyn@gmail.com>
2026-08-23 16:41:09 +0100

    Delete src/src directory

--- src/src/i18n.ts
+++
@@ -1,6 +0,0 @@
-import { getRequestConfig } from 'next-intl/server';
-import { locales } from './i18n/config';
-
-export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`../messages/${locale}.json`)).default
-}));
